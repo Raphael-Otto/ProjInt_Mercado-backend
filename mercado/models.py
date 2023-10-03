463ad7e3541f76d7a339c356aa69bc64ad0a205f
Raphael-Otto <Raphaelotto.RO@gmail.com>
2023-10-03 13:24:15 -0300

    Class Marca

--- mercado/models.py
+++ mercado/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
     def __str__(self):
-        return self.descricao
+        return self.nome
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=100)
+    nacionalidade = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
