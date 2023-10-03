4ea30aeb78156ef26c0494c3b87215de76eccf72
Raphael.otto <Raphaelotto.RO@gmail.com>
2023-10-03 14:48:04 -0300

    Serializer Categoria e Marca

--- mercado/views.py
+++ mercado/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from mercado.models import Categoria, Marca
+from mercado.serializers import CategoriaSerializer, MarcaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
