b1f531f1ff2facdfc73faa6354be9bcb63402f70
Raphael-Otto <Raphaelotto.RO@gmail.com>
2023-11-17 11:03:05 -0300

    Arrumei bebidas

--- mercado/serializers/bebida.py
+++ mercado/serializers/bebida.py
@@ -3,8 +3,20 @@ from django.db import models
 from rest_framework.serializers import ModelSerializer
 from mercado.models import Bebida
 
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+
 class BebidaSerializer(ModelSerializer):
     class Meta:
         model = Bebida
         fields = "__all__"
         depth = 1
+        
+    capa_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
